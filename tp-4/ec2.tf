550829d9b86fc1895492514e3650d19c9dc53366
Francis ABODO <abodojustin@gmail.com>
2023-07-29 16:42:18 +0100

    fix: secret and access key

--- tp-4/ec2.tf
+++ tp-4/ec2.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "myec2" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instancetype
-  key_name      = "test-francis"
+  key_name      = "terraform-test"
   tags          = var.aws_common_tag
   root_block_device {
     delete_on_termination = true
@@ -33,7 +33,7 @@ resource "aws_instance" "myec2" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file("./test-francis.pem")
+      private_key = file("./terraform-test.pem")
       host        = self.public_ip
     }
 
@@ -42,7 +42,7 @@ resource "aws_instance" "myec2" {
 }
 
 resource "aws_security_group" "allow_ssh_http_https" {
-  name        = "francis-sg"
+  name        = "francis-admin-sg"
   description = "Allow http and https inbound traffic"
 
   ingress {
@@ -82,7 +82,7 @@ resource "aws_eip" "lb" {
   vpc      = true
 
   provisioner "local-exec" {
-    command = "echo PUBLIC IP: ${aws_eip.lb.public_ip} ; ID: ${aws_instance.myec2.id} ; AZ: ${aws_instance.myec2.availability_zone}; >> infos_ec2.txt"
+    command = "echo PUBLIC IP: ${aws_eip.lb.public_ip} - ID: ${aws_instance.myec2.id} - AZ: ${aws_instance.myec2.availability_zone} >> infos_ec2.txt"
   }
 
 }
